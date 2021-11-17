2d3001666cfff725b96dc80d4d9c11a7fc149bc5
qystudio0 <78479629+qystudio0@users.noreply.github.com>
2021-11-17 19:49:22 +0800

    Update configs.py

--- configs.py
+++ configs.py
@@ -5,7 +5,7 @@ configuration your MongoDB database here
 
 CONFIGS = {
     'DOMAINS': ['qexo.qystudio.ltd', 'setting.qystudio.ltd', '127.0.0.1'],   # allow hosts
-    'MONGODB_HOST': mongodb+srv://qystudio:ljs070112@cluster0.3ldqe.mongodb.net',  # your MongoDB host
+    'MONGODB_HOST': mongodb+srv://cluster0.3ldqe.mongodb.net',  # your MongoDB host
     'MONGODB_PORT': 27017,  # default is '27017'
     'MONGODB_USER': 'user',  # your MongoDB username
     'MONGODB_DB': 'Cluster0',  # your MongoDB database name

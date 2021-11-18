e162fb1b5a6940638bddbc02fb15c614d8d32f42
Abudu <abudulin@foxmail.com>
2021-11-18 23:16:58 +0800

    feat: 修改 Session 过期时间为一天

--- core/settings.py
+++ core/settings.py
@@ -126,8 +126,9 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SESSION_COOKIE_AGE = 86400

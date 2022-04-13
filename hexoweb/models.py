2627d4f48f140786a62ec6c6c8be3359da0a1089
Abudu <abudulin@foxmail.com>
2022-04-13 23:03:10 +0800

    feat: 注册StatisticModel

--- hexoweb/models.py
+++ hexoweb/models.py
@@ -44,3 +44,9 @@ class CustomModel(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=0x7FFFFFFF)
     content = models.CharField(max_length=0x7FFFFFFF, blank=True)
+
+
+class StatisticModel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=0x7FFFFFFF)
+    content = models.CharField(max_length=0x7FFFFFFF, blank=True)

e27590327c15d44c7766d0e38f12fd5912301c83
Abudu <abudulin@foxmail.com>
2022-05-18 20:03:41 +0800

    fix: 更换默认CDN为unpkg #153

--- hexoweb/functions.py
+++ hexoweb/functions.py
@@ -45,8 +45,8 @@ def get_cdn():
     try:
         cdn_prev = SettingModel.objects.get(name="CDN_PREV").content
     except:
-        save_setting("CDN_PREV", "https://cdn.jsdelivr.net/npm/")
-        cdn_prev = "https://cdn.jsdelivr.net/npm/"
+        save_setting("CDN_PREV", "https://unpkg.com/")
+        cdn_prev = "https://unpkg.com/"
     return cdn_prev
 
 
@@ -78,14 +78,14 @@ def get_custom_config():
         context["QEXO_LOGO"] = SettingModel.objects.get(name="QEXO_LOGO").content
     except:
         save_setting('QEXO_LOGO',
-                     'https://cdn.jsdelivr.net/npm/qexo-static@1.4.0/assets' +
+                     'https://unpkg.com/qexo-static@1.4.0/assets' +
                      '/img/brand/qexo.png')
         context["QEXO_LOGO"] = SettingModel.objects.get(name="QEXO_LOGO").content
     try:
         context["QEXO_ICON"] = SettingModel.objects.get(name="QEXO_ICON").content
     except:
         save_setting('QEXO_ICON',
-                     'https://cdn.jsdelivr.net/npm/qexo-static@1.4.0/assets' +
+                     'https://unpkg.com/qexo-static@1.4.0/assets' +
                      '/img/brand/favicon.ico')
         context["QEXO_ICON"] = SettingModel.objects.get(name="QEXO_ICON").content
     return context

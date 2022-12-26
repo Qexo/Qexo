b117a724d303b5c15d0569e00c63503a386679b4
Abudu <abudulin@foxmail.com>
2022-12-26 22:53:21 +0800

    fix: 修复Elevator

--- hexoweb/libs/elevator/elevator.py
+++ hexoweb/libs/elevator/elevator.py
@@ -1,3 +1,5 @@
+import logging
+
 def parse_version(version):
     return tuple(
         map(int, (version.split(".") if len(version.split(".")) == 3 else version.split(".") + ["0"]))) if version != "false" else None
@@ -13,17 +15,8 @@ def elevator(from_version, to_version):
             for j in range(from_version[1], to_version[1] + 1):
                 for k in range(from_version[2], to_version[2] + 1):
                     try:
-                        exec(f"from {i}.{j}.{k} import elevator")
-                        elevator()
-                    except:
-                        pass
-    else:
-        for i in range(from_version[0], to_version[0] - 1, -1):
-            for j in range(from_version[1], to_version[1] - 1, -1):
-                for k in range(from_version[2], to_version[2] - 1, -1):
-                    try:
-                        exec(f"from elevator.{i}.{j}.{k} import elevator")
-                        elevator()
+                        logging.info("Elevator: %s.%s.%s" % (i, j, k))
+                        exec(f"from {i}_{j}_{k} import elevator")
                     except:
                         pass
     return

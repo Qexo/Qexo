41c3a16144a6864936794a220f346de28c9ae3a7
Abudu <abudulin@foxmail.com>
2023-01-24 23:42:45 +0800

    fix: 在更新脚本中添加CDNJS源

--- hexoweb/libs/elevator/updater/2_5_0.py
+++ hexoweb/libs/elevator/updater/2_5_0.py
@@ -1,13 +1,19 @@
-# 更换Vditor的默认引用CDN
 import logging
+from hexoweb.functions import get_setting, save_setting
+import json
 
 logging.info("开始执行2.5.0版本的更新程序")
 
-from hexoweb.functions import get_setting, save_setting
-
+# 更换Vditor的默认引用CDN
 cdn_prev = get_setting('CDN_PREV')
 logging.info("获取到CDN_PREV => " + cdn_prev)
 if cdn_prev in ["https://unpkg.com/", "https://cdn.jsdelivr.net/npm/"]:
     save_setting('CDN_PREV', "https://npm.onmicrosoft.cn/")
+
+# 添加 渺软公益 CDN
+all_cdnjs = json.loads(get_setting("ALL_CDN"))
+logging.info("ALL_CDN => " + all_cdnjs)
+all_cdnjs.append({"name": "渺软公益 CDN", "url": "https://cdnjs.onmicrosoft.cn/ajax/libs/"})
+save_setting("ALL_CDN", json.dumps(all_cdnjs))
 
 logging.info("执行完成2.5.0版本的更新程序")

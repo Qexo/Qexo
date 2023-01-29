d9158278ae5c2aa9692890c1067c9f4f9a240d2a
Teror Fox <99103591+sysfox@users.noreply.github.com>
2023-01-29 15:07:50 +0800

    Update configs.py

--- hexoweb/libs/platforms/configs.py
+++ hexoweb/libs/platforms/configs.py
@@ -29,5 +29,34 @@ configs = {
             "depth": [1, 2, 1, 1],
             "type": [".yml", ".yaml"]
         }
+    },
+    "Hugo": {
+        "name": "Hugo",
+        "posts": {
+            "path": ["content/posts/"],
+            "depth": [2],
+            "type": [".md"],
+            "save_path": "content/posts/${filename}/index.md",
+            "scaffold": "scaffolds/post.md"
+        },
+        "drafts": {
+            "path": ["source/_drafts"],
+            "depth": [-1],
+            "type": [".md"],
+            "save_path": "source/_drafts/${filename}",
+            "scaffold": "scaffolds/draft.md"
+        },
+        "pages": {
+            "path": ["content"],
+            "depth": [2],
+            "type": ["index.md", "index.html"],
+            "save_path": "content/${filename}/index.md",
+            "scaffold": "scaffolds/page.md"
+        },
+        "configs": {
+            "path":  ["", "themes"],
+            "depth": [1, 4,],
+            "type": [".yml", ".yaml", ".toml"]
+        }
     }
 }    

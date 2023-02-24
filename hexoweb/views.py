e7eee168aec75e0b48577f50d2911d0d266de41b
Abudu <abudulin@foxmail.com>
2023-02-24 23:30:38 +0800

    fix: 修复前端传值错误

--- hexoweb/views.py
+++ hexoweb/views.py
@@ -533,7 +533,7 @@ def pages(request):
                     posts = json.loads(cache.first().content)
                 else:
                     posts = update_pages_cache(search)
-            context["posts"] = posts
+            context["posts"] = json.dumps(posts)
             context["post_number"] = len(posts)
             context["page_number"] = ceil(context["post_number"] / 15)
             context["search"] = search
@@ -554,7 +554,7 @@ def pages(request):
                     posts = json.loads(cache.first().content)
                 else:
                     posts = update_configs_cache(search)
-            context["posts"] = posts
+            context["posts"] = json.dumps(posts)
             context["post_number"] = len(posts)
             context["page_number"] = ceil(context["post_number"] / 15)
             context["search"] = search
@@ -579,7 +579,7 @@ def pages(request):
                                   "time": strftime("%Y-%m-%d %H:%M:%S", localtime(int(i.time))),
                                   "like": len(t) if t else 0,
                                   "id": i.id.hex})
-            context["posts"] = sorted(posts, key=lambda x: x["time"], reverse=True)
+            context["posts"] = json.dumps(sorted(posts, key=lambda x: x["time"], reverse=True))
             context["post_number"] = len(posts)
             context["page_number"] = ceil(context["post_number"] / 15)
             context["search"] = search
@@ -600,7 +600,7 @@ def pages(request):
                                   "date": strftime("%Y-%m-%d %H:%M:%S",
                                                    localtime(float(i.date))),
                                   "time": i.date})
-            context["posts"] = posts[::-1]
+            context["posts"] = json.dumps(posts[::-1])
             context["post_number"] = len(posts)
             context["page_number"] = ceil(context["post_number"] / 15)
             context["search"] = search
@@ -738,6 +738,7 @@ def pages(request):
                 context["post_number"] = len(context["posts"])
                 context["page_number"] = ceil(context["post_number"] / 15)
                 context["all_posts"] = json.dumps(context["posts"])
+                context["posts"] = json.dumps(context["posts"])
             except Exception as e:
                 logging.error("获取错误: " + repr(e))
                 context["error"] = repr(e)

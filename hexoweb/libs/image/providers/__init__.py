b490afca6ef74d82bc29c6fb49667024b6b90e26
Abudu <abudulin@foxmail.com>
2023-04-04 20:13:13 +0800

    feat(image): 支持阿里云OSS

--- hexoweb/libs/image/providers/__init__.py
+++ hexoweb/libs/image/providers/__init__.py
@@ -7,6 +7,7 @@ from . import custom
 from . import s3
 from . import ftp
 from . import dogecloudoss
+from . import alioss
 from . import gitHub
 
 _all_providers = {
@@ -14,5 +15,6 @@ _all_providers = {
     s3.S3.name: s3.S3,
     ftp.Ftp.name: ftp.Ftp,
     dogecloudoss.DogeCloudOss.name: dogecloudoss.DogeCloudOss,
+    alioss.AliOss.name: alioss.AliOss,
     gitHub.Github.name: gitHub.Github
 }

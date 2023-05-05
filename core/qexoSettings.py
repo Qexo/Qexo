0edccf55e09c78ae7aa4cc53bc5fc8373c32ab16
Abudu <abudulin@foxmail.com>
2023-05-05 13:38:12 +0800

    chore(release):2.8.1

--- core/qexoSettings.py
+++ core/qexoSettings.py
@@ -1,7 +1,7 @@
 import json
 import random
 
-QEXO_VERSION = "2.8.0"
+QEXO_VERSION = "2.8.1"
 
 DEFAULT_EMOJI = {"微笑": "🙂", "撇嘴": "😦", "色": "😍", "发呆": "😍", "得意": "😎",
                  "流泪": "😭", "害羞": "😊", "闭嘴": "😷", "睡": "😴",

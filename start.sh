2de20b559a9476109894dea8ea7cb0bcc095c743
DingJunyao <dingjunyao0703@163.com>
2023-09-12 19:33:06 +0800

    add check script

--- start.sh
+++ start.sh
@@ -1,3 +1,19 @@
 # /bin/bash
+if [ ! -s /blog ]
+then
+    echo "未发现 Hexo 目录下有内容，自动初始化..."
+    cd / && \
+    hexo init blog && \
+    cd blog && \
+    npm i && \
+    hexo g -d
+    cd /app
+fi
+if [ ! -f /db/qexo_data.db ]
+then
+    echo "未发现 Qexo 数据库文件，自动初始化..."
+    python3 manage.py makemigrations && \
+    python3 manage.py migrate
+fi
 caddy file-server --listen :3000 --root /blog/public &
 python3 manage.py runserver 0.0.0.0:8000 --noreload

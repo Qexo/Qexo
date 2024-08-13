1324237443f1fef9083f18e88d09b99aace2097a
Abudu <abudulin@foxmail.com>
2024-08-14 00:27:21 +0800

    feat(i18n): 添加en_UK

--- hexoweb/libs/i18n/languages/__init__.py
+++ hexoweb/libs/i18n/languages/__init__.py
@@ -6,6 +6,7 @@
 from . import zh_CN
 from . import zh_TW
 from . import en_US
+from . import en_UK
 from . import fr_FR
 from . import ja_JP
 from . import ko_KR
@@ -13,6 +14,7 @@ from . import ko_KR
 _all_languages = {
     zh_CN.Main.name: zh_CN,
     zh_TW.Main.name: zh_TW,
+    en_UK.Main.name: en_UK,
     en_US.Main.name: en_US,
     fr_FR.Main.name: fr_FR,
     ja_JP.Main.name: ja_JP,

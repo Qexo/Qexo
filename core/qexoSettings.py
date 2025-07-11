17ca6dcf927a5594b94379e5404edcc821426dbd
Abudu <abudulin@foxmail.com>
2025-07-11 11:01:48 +0800

    feat: 支持从 cnb 一键更新

--- core/qexoSettings.py
+++ core/qexoSettings.py
@@ -46,8 +46,8 @@ DEFAULT_CDN = [
 DEFAULT_UPDATES = [
     {"name": "master", "url": "https://github.com/Qexo/Qexo/tarball/master/"},
     {"name": "dev", "url": "https://github.com/Qexo/Qexo/tarball/dev/"},
-    {"name": "master_ghproxy", "url": "https://mirror.ghproxy.com/https://github.com/Qexo/Qexo/archive/master.tar.gz"},
-    {"name": "dev_ghproxy", "url": "https://mirror.ghproxy.com/https://github.com/Qexo/Qexo/archive/dev.tar.gz"}
+    {"name": "master_cnb", "url": "https://cnb.cool/qexo/Qexo/-/git/archive/master.tar.gz"},
+    {"name": "dev_cnb", "url": "https://cnb.cool/qexo/Qexo/-/git/archive/dev.tar.gz"}
 ]
 
 ALL_SETTINGS = [  # [名称, 默认值, 是否在尝试修复时重置, 简介]
